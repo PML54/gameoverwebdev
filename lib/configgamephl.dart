9fda68fc6bedfa010b94a710c7229ccc3929d44b
Paul de Pontoise <35325632+PML54@users.noreply.github.com>
2022-07-01 08:01:06 +0200

    Add files via upload

--- lib/configgamephl.dart
+++ lib/configgamephl.dart
@@ -2,7 +2,8 @@ import 'dart:core';
 const String  prefixPhoto ="upoad/PML_01_"; // Syntaxe
 const   String  unknownCodeMaster = "Code Incorrect";
 
-const String pathPHP= "https://lamemopole.com/php/";
+//const String pathPHP= "https://lamemopole.com/php/"; // PROD
+const String pathPHP= "https://paulbrode.com/php/";  //DEV
 List statusGame = ["CREATED" , "PHOTOCLOSED" , "INVITECLOSED","MEMECLOSED", "VOTECLOSED"];
 List modeGame =  ["PUBLIC" , "PRIVATE"];
 List msgNewGame = ["Nom Game ?" , "Photos Selected ? "];
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "dd31fb2b9c02ad1d80603d76857a48ef",
-"index.html": "04af9a8daf5ae22641127b57944ae1b7",
-"/": "04af9a8daf5ae22641127b57944ae1b7",
-"main.dart.js": "cc4555c8359352e02b68cffe5d0935f2",
+"index.html": "d74896c894912d11200165061dc22d0e",
+"/": "d74896c894912d11200165061dc22d0e",
+"main.dart.js": "bd3fb3617af60b3b23bf0c3910b17649",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
